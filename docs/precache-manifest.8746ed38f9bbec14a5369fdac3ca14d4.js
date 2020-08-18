40b42818d2c996e9ac7e26588a5f9fbdfb526c9e
rraulg5 <rraulg5@gmail.com>
2020-08-17 19:02:24 -0500

    docs added

---
+++ docs/precache-manifest.8746ed38f9bbec14a5369fdac3ca14d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead5a52b0a67bed5a1415de71d05df72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2276c492c079821496c",
+    "url": "/static/css/main.06d6387c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2276c492c079821496c",
+    "url": "/static/js/main.bf79b87b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
